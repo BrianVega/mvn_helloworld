e7e11a0919085581eb8da26305c9c6780d8bf99e
Brian Vega <BrianVega01@outlook.es>
2024-07-25 12:44:26 -0600

    Maven Surefire plugin - pojotest

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+public class HelloWorldTest {
+    public void testHello() {
+        System.out.println("TEST - Hello... ");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World... ");
+    }
+
+    public void nonExecutableTestX() {
+        System.out.println("TEST - NonExecutableTestX... ");
+    }
+}
